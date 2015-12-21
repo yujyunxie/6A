4f38566bc29b90635bd4e3dff98640e1b8173c06
yujyunxie <yujyunxie@gmail.com>
2015-12-21 17:06:57 +0800

    Second Version

--- src/Question.java
+++ src/Question.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Question {
 
@@ -10,4 +11,26 @@ public class Question {
 
 	}
 
+}
+
+interface quiz {
+	abstract void showquiz();
+}
+
+class problem implements quiz {
+	String description;
+	public problem(String desc) {
+		this.description = desc;
+	}
+	public void showquiz() {
+		System.out.println("sentence");
+	}
+}
+
+class paper implements quiz {
+	ArrayList<quiz> q = new ArrayList<quiz>();
+	
+	public void showquiz() {
+		System.out.println("sentence2");
+	}
 }
